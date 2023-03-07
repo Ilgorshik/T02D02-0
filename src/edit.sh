ad61f990cd65a8a31507abf1ab8ef4cde3ac2061
Runcel Kaye <runcelka@ev-c4.kzn.21-school.ru>
2023-03-07 16:38:28 +0300

    Квест 1 и 2

---
+++ src/edit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+  echo "No arguments"
+  exit
+fi
+
+if [ -z "$2" ]; then
+  echo "Нет аргумента 2"
+fi
+
+if [ -z "$3" ]; then
+  echo "Нет аргумента 3"
+  exit
+fi
+if test -f "$1";then
+sed -i -e "s/$2/$3/" $1
+echo "src/$1 - $(stat $1 | awk '{print $8}') - $(date +%F) $(date +%H:%M) - $(shasum -a256 $1  | awk '{print $1}') - sha256" >> temp.log
+else
+  echo "$1 не существует"
+fi
